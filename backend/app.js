b33ca9eaedee8a5da23acc160fe7e493b8d480ee
Rafal Byczek <zsen.1bt.byczek@gmail.com>
2024-01-19 23:06:25 +0100

    endpoint strzał

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
+const express = require('express');
+const app = express();
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+
+let data; 
+const port = 3000;
 
 
 const sqlite3 = require('sqlite3').verbose();
@@ -14,11 +18,11 @@ let db = new sqlite3.Database('../database/music.db', (err) => {
 });
 
 db.serialize(() => {
-    db.each(`SELECT title FROM tracks`, (err, row) => {
+    db.each(`SELECT * FROM tracks`, (err, row) => {
         if (err) {
             console.error(err.message);
         }
-        console.log(row);
+        data = row;
     });
 });
 
@@ -28,3 +32,19 @@ db.close((err) => {
     }
     console.log('Zamknięto połączenie z bazą danych.');
 });
+
+app.get('/data', function(req, res){
+    res.send(data)
+    console.log('xxxx')
+})
+
+app.post('/endpoint', function(req, res){
+    res.send('Endpoint post req')
+})
+
+app.get("/dudu", (req, res) => res.send("Hello world"));
+
+app.listen(port, ()=> {
+    console.log(`App słucha http://loclhost:${port}`)
+})
+
