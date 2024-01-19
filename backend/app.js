b43e194cc6d0ce6108778602ff660c2b1fa64269
tma19993 <tmazzurek@gmail.com>
2024-01-19 22:41:22 +0100

    connection between database and backend

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,29 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+
+
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('../database/music.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Połączono z bazą danych.');
+});
+
+db.serialize(() => {
+    db.each(`SELECT title FROM tracks`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(row);
+    });
+});
+
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Zamknięto połączenie z bazą danych.');
+});
