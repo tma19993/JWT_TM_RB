af893a5521175827b0021f8e223c067769e9f133
Rafal Byczek <zsen.1bt.byczek@gmail.com>
2024-01-19 14:45:03 +0100

    fix

--- projektangular/my-angular-project/src/app/app.component.ts
+++ projektangular/my-angular-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-angular-project';
+  tracks: any;
 }
